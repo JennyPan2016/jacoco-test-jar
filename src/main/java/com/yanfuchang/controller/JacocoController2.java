34feb2d44161bb9da3718b020c092bf77a547522
panjing184 <panjing184@pingan.com.cn>
2020-10-23 21:23:38 +0800

    update

--- src/main/java/com/yanfuchang/controller/JacocoController2.java
+++ src/main/java/com/yanfuchang/controller/JacocoController2.java
@@ -20,8 +20,8 @@ public class JacocoController2 {
     ){
         if ("daling".equals(jacoco.getData())) {
             return "daling";
-        }else if ("dalingjia".equals(jacoco.getData())){
-            return "dalingjia";
+        }else if ("dalingjia2".equals(jacoco.getData())){
+            return "dalingjia2";
         }else if ("test2".equals(jacoco.getData())){
             return "add test2";
         }else if ("test3".equals(jacoco.getData())){
